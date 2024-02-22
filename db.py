7885574e6bf6afd6b259661c5ff0d9e63d224ac0
ahmed-aouad <ahmed.aouad@laplateforme.io>
2024-02-22 11:03:14 +0100

    db and main amélioration

--- db.py
+++ db.py
@@ -1,22 +1,26 @@
 # db.py
 
-import mysql.connector
+import mysql.connector  # Importation du module pour la connexion MySQL
 
 class Database:
     def __init__(self, host, user, password, database):
         try:
+            # Tentative de connexion à la base de données avec les paramètres fournis
             self.connection = mysql.connector.connect(
                 host=host,
                 user=user,
                 password=password,
                 database=database
             )
+            # Si la connexion réussit, afficher un message de confirmation
             print("Connexion à la base de données réussie")
         except mysql.connector.Error as e:
+            # En cas d'erreur lors de la connexion, afficher un message d'erreur
             print(f"Erreur de connexion à la base de données: {e}")
-            self.connection = None
+            self.connection = None  # Définir la connexion à None en cas d'échec
 
     def get_connection(self):
-        return self.connection
+        # Fonction pour obtenir la connexion à la base de données
+        return self.connection  # Retourne l'objet de connexion
 
 
--- main.py
+++ main.py
@@ -8,23 +8,33 @@ from canal import Canal
 from chat_window import ChatWindow
 
 def main():
+    # Établir la connexion à la base de données
     db = Database('ahmed-aouad.students-laplateforme.io', 'ahmed-aouad', 'ouarda2017', 'ahmed-aouad_mydiscord')
     db_connection = db.get_connection()
 
+    # Initialiser l'application Qt
     app = QApplication(sys.argv)
 
+    # Vérifier si la connexion à la base de données est réussie
     if db_connection is not None:
+        # Créer un nouvel utilisateur
         utilisateur = Utilisateur('Jean', 'Dupont', 'jean.dupont@email.com')
+        # Ajouter l'utilisateur à la base de données avec un mot de passe
         utilisateur.ajouter_utilisateur(db_connection, 'motdepasse')
 
+        # Créer un objet Canal pour gérer les canaux de discussion
         canal = Canal(db_connection)
 
-        chat_window = ChatWindow(utilisateur, canal, db_connection)  # Passez db_connection ici
+        # Créer une fenêtre de chat avec l'utilisateur, le canal et la connexion à la base de données
+        chat_window = ChatWindow(utilisateur, canal, db_connection)
+        # Afficher la fenêtre de chat
         chat_window.show()
 
+    # Lancer l'exécution de l'application Qt
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
+    # Appeler la fonction main pour démarrer l'application
     main()
 
 
